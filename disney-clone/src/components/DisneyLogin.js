10bf37e58ba570357fc07ac148a592f40c5a22b6
Khushboo <patelkhushboo057@gmail.com>
2023-12-06 16:46:05 +0530

    center signup section completed

--- disney-clone/src/components/DisneyLogin.js
+++ disney-clone/src/components/DisneyLogin.js
@@ -3,14 +3,22 @@ import styled, { css } from 'styled-components';
  export const DisneyLogin = () => {
   return (
     <Container>
-        <Content>
-            <CTA>
-                <CTALogoOne src='/images/cta-logo-one.svg' alt=''/>
-            </CTA>
-            <BgImage />
-        </Content>
+      <Content>
+        <CTA>
+          <CTALogoOne src="/images/cta-logo-one.svg" alt="" />
+          <SignUp>GET ALL HERE</SignUp>
+          <Description>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book.
+          </Description>
+          <CTALogoTwo src="/images/cta-logo-two.png" alt="" />
+        </CTA>
+        {/* <BgImage /> */}
+      </Content>
     </Container>
-  )
+  );
 }
 
 const Container= styled.section`
@@ -22,6 +30,7 @@ const Container= styled.section`
 
 `;
 const Content= styled.div`
+background-image: url(/images/login-background.jpg);
 margin-bottom: 10vw;
 width: 100%;
 position: relative;
@@ -33,20 +42,21 @@ flex-direction: column;
 padding: 80px 40px;
 height: 100%;`;
 
-const BgImage = styled.div`
-background-image: url(/images/login-background.jpg);
-height: 100%;
-background-position: top;
-background-size: cover;
-background-repeat: no-repeat;
-position: absolute;
-top: 0;
-right: 0;
-left: 0;
-z-index=-1;
-`;
+// const BgImage = styled.div`
+
+// height: 100%;
+// background-position: top;
+// background-size: cover;
+// background-repeat: no-repeat;
+// position: absolute;
+// top: 0;
+// right: 0;
+// left: 0;
+
+// `;
 
 const CTA = styled.div`
+
 margin-bottom: 2vw;
     max-width: 650px;
     flex-wrap: wrap;
@@ -64,10 +74,43 @@ margin-bottom: 2vw;
 `;
 const CTALogoOne = styled.img`
 margin-bottom: 12px;
-    position: absolute;
-    z-index: 2;
+    
+   
     max-width: 600px;
-    min-height: 1px;
+   
     display: block;
     width: 100%;
 `;
+
+const SignUp= styled.a`
+font-weight: bold;
+    color: #ffffff;
+    background-color: #0063e5;
+    width: 100%;
+    font-size: 18px;
+    margin-bottom: 12px;
+    letter-spacing: 1.5px;
+    padding: 16.5px 0;
+    border: 1px solid transparent;
+    border-radius: 4px;
+    &:hover{
+        background-color: #0483ee;
+    }
+
+
+`;
+
+const Description = styled.p`
+color: hsla(0,0%,95.3%,1);
+    font-size: 11px;
+    margin: 0 0 24px;
+    line-height: 1.5;
+    letter-spacing: 1.5px;
+`;
+
+const CTALogoTwo = styled.img`
+margin-bottom: 20px;
+    max-width: 600px;
+    display: inline-block;
+    vertical-align: bottom;
+    width: 100%;`;
